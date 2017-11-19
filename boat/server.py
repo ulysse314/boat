2e95c445b4fec35488fe9dd0638e36c44c33b231
moussaillon <moussaillon>
2017-11-19 16:22:37 +0100

    Updating boat server

--- boat/server.py
+++ boat/server.py
@@ -12,9 +12,8 @@ import sys
 os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
 sys.path.append("..")
 
-import camera
 import config
-import value_logger
+import value_sender
 
 BOAT_NAME = sys.argv[1]
 CONFIG_FILE = sys.argv[2]
@@ -23,3 +22,10 @@ logging.basicConfig(level=logging.DEBUG)
 config.load(CONFIG_FILE, BOAT_NAME)
 
 BOAT_PORT = int(config.values["boat_port"])
+RELAY_SERVER = config.value["value_relay_server"]
+
+sender = value_sender.ValueSender(BOAT_NAME, RELAY_SERVER, BOAT_PORT, config.values["boat_key"])
+
+loop = asyncio.get_event_loop()
+loop.create_task(sender.run())
+loop.run_forever()
