5c8a2bfe5b9e9cf7a4600bca19adbb28f5995990
moussaillon <moussaillon>
2017-12-17 06:00:17 +0100

    Moving from ulysse314.ini to ulysse314.json, as the config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import configparser
+import json
 import pprint
 import sys
 
@@ -6,13 +6,13 @@ values = None
 
 def load(boat_name):
   global values
-  parser = configparser.SafeConfigParser()
-  parser.read("/etc/ulysse314/ulysse314.ini")
-
-  if values == None:
-    values = {}
-    for name, value in parser.items("shared"):
-      values[name] = value
-    for name, value in parser.items(boat_name):
-      values[name] = value
+  if values:
+    return values
+  values = {}
+  with open("/etc/ulysse314/ulysse314.json") as data_file:
+    loaded_json = json.load(data_file)
+    for key in loaded_json["shared"]:
+      values[key] = loaded_json["shared"][key]
+    for key in loaded_json["boats"][boat_name]:
+      values[key] = loaded_json["boats"][boat_name][key]
   return values
