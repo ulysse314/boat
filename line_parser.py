56c231ba67f5ee06c098da3c51474dcaee8616cb
moussaillon <moussaillon>
2018-02-28 19:05:22 +0100

    Should support to parse empty lines

--- line_parser.py
+++ line_parser.py
@@ -5,8 +5,10 @@ import logging
 import pprint
 
 class LineParser:
-  partial_packet = b''
-  logger = logging.getLogger("LineParser")
+  def __init__(self, accept_empty_line = False):
+    self.partial_packet = b''
+    self.logger = logging.getLogger("LineParser")
+    self.accept_empty_line = accept_empty_line
 
   def add_buffer(self, buffer):
     result = []
@@ -23,13 +25,20 @@ class LineParser:
         end_of_buffer_index -= 1
       full_packet = self.partial_packet[:end_of_buffer_index]
       self.partial_packet = self.partial_packet[eol_index + 1:]
-      if len(full_packet) > 0:
+      if len(full_packet) > 0 or self.accept_empty_line:
         result.append(full_packet)
     return result
 
 if __name__ == '__main__':
   line_parser = LineParser()
-  list = line_parser.add_buffer(b'toto\r\ntata')
+  list = line_parser.add_buffer(b'toto\r\n\r\ntata')
+  pprint.pprint(list)
+  list = line_parser.add_buffer(b'toto\n\rtata')
+  pprint.pprint(list)
+  list = line_parser.add_buffer(b'x\ny\n')
+  pprint.pprint(list)
+  line_parser = LineParser(True)
+  list = line_parser.add_buffer(b'toto\r\n\r\ntata')
   pprint.pprint(list)
   list = line_parser.add_buffer(b'toto\n\rtata')
   pprint.pprint(list)
