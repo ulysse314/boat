be390b4bd939ba0bddce98f4dd4ac0431d2c7b78
moussaillon <moussaillon>
2018-04-10 09:21:34 +0200

    Adding actuator

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,17 @@
-#include "SensorList.h"
+#include "ActuatorList.h"
 #include "GPSSensor.h"
+#include "SensorList.h"
 
 SensorList sensorList;
+ActuatorList activatorList;
 
 void setup() {
   GPSSensor *gpsSensor = new GPSSensor();
   sensorList.addSensor(gpsSensor);
   sensorList.begin();
+  sensorList.loop();
+  activatorList.begin();
+  activatorList.loop();
   while (!Serial);
   Serial.begin(115200);
 }
@@ -21,4 +26,6 @@ void loop() {
     sensorList.printInfo(&Serial, 0);
     Serial.println("--");
   }
+  activatorList.loop();
+  activatorList.listen(&Serial);
 }
