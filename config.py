7a35136de68e1cd53533f2fc2d5ec57d2abc9d2c
moussaillon <moussaillon>
2018-04-17 02:22:37 +0200

    Adding config.default_boat_name(), and config.load() can use the default boat name

--- config.py
+++ config.py
@@ -2,17 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 import pprint
 import sys
 
+SETTINGS_DIR = "/etc/ulysse314/"
 values = None
 
-def load(boat_name):
+def default_boat_name():
+  global SETTINGS_DIR
+  with open(os.path.join(SETTINGS_DIR, "name"), "r") as file:
+    boat_name = file.readline().strip()
+  return boat_name
+
+def load(boat_name = None):
   global values
+  global SETTINGS_DIR
+  if not boat_name:
+    boat_name = default_boat_name()
   if values:
     return values
   values = {}
-  with open("/etc/ulysse314/ulysse314.json") as data_file:
+  json_file = os.path.join(SETTINGS_DIR, "ulysse314.json")
+  with open(json_file) as data_file:
     loaded_json = json.load(data_file)
     for key in loaded_json["shared"]:
       values[key] = loaded_json["shared"][key]
