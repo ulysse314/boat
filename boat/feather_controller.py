7412244e1a5a035d1d14a9bff9dd37da5d5a3cbe
moussaillon <moussaillon>
2018-05-06 17:01:39 +0200

    Should be 'motor' instead of 'motors' in the json exchanged

--- boat/feather_controller.py
+++ boat/feather_controller.py
@@ -91,7 +91,7 @@ class FeatherController:
       await asyncio.sleep(1)
 
   def _update_values(self):
-    self.values = { "gps": {}, "motors": {} }
+    self.values = { "gps": {}, "motor": {} }
     for sensor_id in self.received_values:
       values = self.received_values[sensor_id]['values']
       if sensor_id == '0 GPS serial1':
@@ -109,10 +109,10 @@ class FeatherController:
         except:
           self.logger.exception("Problem to get gps")
       elif sensor_id == "0 Motor Motor":
-        self.values["motors"]["left%"] = values[3]
-        self.values["motors"]["right%"] = values[4]
-        self.values["motors"]["leftresult"] = values[5]
-        self.values["motors"]["rightresult%"] = values[6]
+        self.values["motor"]["left%"] = values[3]
+        self.values["motor"]["right%"] = values[4]
+        self.values["motor"]["leftresult"] = values[5]
+        self.values["motor"]["rightresult%"] = values[6]
       elif sensor_id in self.sensors:
         sensor = self.sensors[sensor_id]
         if sensor["type"] == "dallas":
