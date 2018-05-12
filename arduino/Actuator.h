5925a4ea2296dfbdacdca520159d5f33895c31ed
moussaillon <moussaillon>
2018-05-12 11:46:50 +0200

    Adding #ifndef for Actuator.h

--- arduino/Actuator.h
+++ arduino/Actuator.h
@@ -1,3 +1,6 @@
+#ifndef Actuator_h
+#define Actuator_h
+
 class Actuator {
 public:
   virtual const char *addressString() const = 0;
@@ -5,3 +8,5 @@ public:
   virtual bool loop() {};
   virtual bool processValues(const char *values) { return true; };
 };
+
+#endif // Actuator_h
