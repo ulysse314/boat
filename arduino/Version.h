75efc1390a880ff66c64a586755f69c779fc0d8b
moussaillon <moussaillon>
2018-05-31 00:09:46 +0200

    Adding support for the boat id in the arduino code

--- arduino/Version.h
+++ arduino/Version.h
@@ -1,1 +1,12 @@
+#ifndef Version_h
+#define Version_h
+
 #define ArduinoVersion 1
+
+#define MOUSSAILLON_ID 314
+#define TELEMAQUE_ID 315
+
+#define IS_MOUSSAILLON (BOAT_ID == MOUSSAILLON_ID)
+#define IS_TELEMAQUE (BOAT_ID == TELEMAQUE_ID)
+
+#endif // Version_h
