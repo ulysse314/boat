a142a47ae8a6eae279c9d52ed965c16b9e929fab
moussaillon <moussaillon>
2018-10-13 15:48:27 +0200

    Fixing a waring when overriding a method

--- arduino/Sensors/MotorActuatorSensor.h
+++ arduino/Sensors/MotorActuatorSensor.h
@@ -12,7 +12,7 @@ public:
   ~MotorActuatorSensor();
 
   const char *addressString() const override { return "Motor"; };
-  const char *sensorType() const { return "Motor"; };
+  const char *sensorType() const override { return "Motor"; };
   const char *sensorClass() const override { return "Motor"; };
   void begin() override;
   bool processValues(const char *values) override;
