91b60493fb9b04bcbacc01bc2498476e7940fcac
moussaillon <moussaillon>
2019-03-20 22:55:30 +0100

    Removing arduino controller (replaced by feather controller)

--- boat/test_server.py
+++ boat/test_server.py
@@ -13,7 +13,6 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if parent_dir not in sys.path:
   sys.path.append(parent_dir)
 
-import arduino_controller
 import boat_controller
 import command_controller
 import config
