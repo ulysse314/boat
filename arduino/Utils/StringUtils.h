bc6cf15a14dbf773fda030715ed75a5342c710af
moussaillon <moussaillon>
2020-01-02 02:11:06 +0100

    Fixing createStringCopy(), it should return NULL when receiving NULL

--- arduino/Utils/StringUtils.h
+++ arduino/Utils/StringUtils.h
@@ -2,6 +2,7 @@
 #define StringUtils_h
 
 inline const char *createStringCopy(const char *string) {
+  if (!string) return NULL;
   size_t length = strlen(string) + 1;
   char *stringCopy = (char *)malloc(length);
   strncpy(stringCopy, string, length);
