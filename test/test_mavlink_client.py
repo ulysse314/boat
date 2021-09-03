56e14084cfebeb52ffaa2907b7a651cf561b3cb3
moussaillon <moussaillon>
2021-09-04 00:37:06 +0100

    Update test_mavlink_client.py to have more parameter

--- test/test_mavlink_client.py
+++ test/test_mavlink_client.py
@@ -6,7 +6,7 @@ import pprint
 import socket
 import sys
 
-sys.path.append("mavlink")
+sys.path.append("../shared/mavlink")
 from MavlinkGenerated import mavlink
 
 class Toto:
@@ -14,9 +14,10 @@ class Toto:
     self.client = client
 
   def write(self, buff):
+    print("send buffer {}".format(len(buff)))
     n = client.send(buff)
 
-server_address = ('127.0.0.1', 5760 )
+server_address = (sys.argv[1], int(sys.argv[2]))
 
 print("Connect to {}".format(server_address))
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,13 +28,14 @@ def receive_message(message):
 #  pprint.pprint(vars(message))
 #  pprint.pprint(dir(message))
   pprint.pprint(message.get_type())
+  pprint.pprint(message.get_seq())
   pprint.pprint(message.id)
-  pprint.pprint(mavlink.MAVLINK_MSG_ID_HEARTBEAT)
   pprint.pprint(message.type)
 
 
 test = mavlink.MAVLink(Toto(client))
 test.set_callback(receive_message)
+client.send("".encode("utf-8"))
 test.heartbeat_send(mavlink.MAV_TYPE_SURFACE_BOAT, mavlink.MAV_AUTOPILOT_GENERIC, mavlink.MAV_MODE_FLAG_SAFETY_ARMED, 0, mavlink.MAV_STATE_BOOT)
 test.heartbeat_send(mavlink.MAV_TYPE_SUBMARINE, mavlink.MAV_AUTOPILOT_GENERIC, mavlink.MAV_MODE_FLAG_SAFETY_ARMED, 0, mavlink.MAV_STATE_BOOT)
 
